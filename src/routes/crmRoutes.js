2c84fe0d270e120fdf6d391dea0f57737879f35e
Dario <darolisn454@gmail.com>
2021-05-03 20:42:14 -0600

    update

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,27 @@
+import { addNewContact } from '../controllers/crmControllers';
+
+const routes = (app) => {
+    app.route('/contact')
+        .get((req, res, next)=>{
+            //middleware
+            console.log(`Request from ${req.originalUrl}`)
+            console.log(`Request type ${req.method}`)
+            next();
+        },(req, res, next)=> {
+
+            res.send("GET request successful")
+        })
+
+
+        .post(addNewContact);
+
+    app.route('/contact/:contact|ID')
+        .put((req, res)=>
+        res.send("PUT request successful"))
+
+        .delete((req, res)=>
+        res.send("DELETE request successful"))
+
+}
+
+export default routes;
